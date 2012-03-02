65f81075cd5ce332dcc70b40c371067f518e50e7
Matthias Kluth <matthias@kluth.me>
2012-03-03 00:05:25 +0100

    Used a working version, now.

--- js/todo.js
+++ js/todo.js
@@ -1,54 +1,68 @@
-var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
+var gtugruhr = {};
+var indexedDB = window.indexedDB || window.webkitIndexedDB ||
+window.mozIndexedDB;
 if ('webkitIndexedDB' in window) {
 	window.IDBTransaction = window.webkitIDBTransaction;
 	window.IDBKeyRange = window.webkitIDBKeyRange;
 }
-var gtugruhr = {};
 gtugruhr.indexedDB = {};
 gtugruhr.indexedDB.db = null;
-gtugruhr.indexedDB.open = function(){
+gtugruhr.indexedDB.onerror = function(e) {
+	console.log(e);
+};
+gtugruhr.indexedDB.open = function() {
 	var request = indexedDB.open("todos");
-	request.onsuccess = function(e){
-		var v = "1.0";
+	request.onsuccess = function(e) {
+		var v = "1.99";
 		gtugruhr.indexedDB.db = e.target.result;
 		var db = gtugruhr.indexedDB.db;
-		if(v != db.version){
+		// We can only create Object stores in a setVersion transaction;
+		if (v!= db.version) {
 			var setVrequest = db.setVersion(v);
-			setVrequest.onfailure = gtugruhr.indexedDB.onerror;
-			setVrequest.onsuccess = function(e){
+			// onsuccess is the only place we can create Object Stores
+			setVrequest.onerror = gtugruhr.indexedDB.onerror;
+			setVrequest.onsuccess = function(e) {
+				if(db.objectStoreNames.contains("todo")) {
+					db.deleteObjectStore("todo");
+				}
 				var store = db.createObjectStore("todo",
-						{keyPath: "timestamp"});
+						{keyPath: "timeStamp"});
 				gtugruhr.indexedDB.getAllTodoItems();
 			};
 		}
-		gtugruhr.indexedDB.getAllTodoItems();
+		else {
+			gtugruhr.indexedDB.getAllTodoItems();
+		}
 	};
-	request.onfailure = gtugruhr.indexedDB.onerror;
-};
-gtugruhr.indexedDB.addTodo = function(todoText){
+	request.onerror = gtugruhr.indexedDB.onerror;
+}
+gtugruhr.indexedDB.addTodo = function(todoText) {
 	var db = gtugruhr.indexedDB.db;
 	var trans = db.transaction(["todo"], IDBTransaction.READ_WRITE);
 	var store = trans.objectStore("todo");
-	var request = store.put({
-			"text": todoText,
-			"timeStamp": new Date().getTime()
-			});
-	request.onsuccess = function(e){
+	if(!todoText) return;
+	var data = {
+		"text": todoText,
+		"timeStamp": new Date().getTime()
+	};
+	var request = store.put(data);
+	request.onsuccess = function(e) {
 		gtugruhr.indexedDB.getAllTodoItems();
 	};
-	request.onerror = function(e){
-		console.log(e.value);
+	request.onerror = function(e) {
+		console.log("Error Adding: ", e);
 	};
 };
-gtugruhr.indexedDB.getAllTodoItems = function(){
+gtugruhr.indexedDB.getAllTodoItems = function() {
 	var todos = document.getElementById("todoItems");
-	todos.inerHTML = "";
+	todos.innerHTML = "";
 	var db = gtugruhr.indexedDB.db;
 	var trans = db.transaction(["todo"], IDBTransaction.READ_WRITE);
 	var store = trans.objectStore("todo");
+	// Get everything in the store;
 	var keyRange = IDBKeyRange.lowerBound(0);
 	var cursorRequest = store.openCursor(keyRange);
-	cursorRequest.onsuccess = function(e){
+	cursorRequest.onsuccess = function(e) {
 		var result = e.target.result;
 		if(!!result == false)
 			return;
@@ -57,22 +71,19 @@ gtugruhr.indexedDB.getAllTodoItems = function(){
 	};
 	cursorRequest.onerror = gtugruhr.indexedDB.onerror;
 };
-function renderTodo(row){
-	var todoItems = document.getElementById("todoItems");
+function renderTodo(row) {
+	var todos = document.getElementById("todoItems");
 	var li = document.createElement("li");
-	var t = document.createTextNode();
-	t.data = row.text;
+	var t = document.createTextNode(row.text);
 	li.appendChild(t);
-	todoItems.appendChild(li);
+	todos.appendChild(li)
 }
 function addTodo() {
-	var todo = document.getElementById('todo-item');
+	var todo = document.getElementById("todo");
 	gtugruhr.indexedDB.addTodo(todo.value);
-	todo.value = '';
+	todo.value = "";
 }
-function init(){
+function init() {
 	gtugruhr.indexedDB.open();
 }
-$(document).ready(function(){
-		init();
-		});
+window.addEventListener("DOMContentLoaded", init, false);
